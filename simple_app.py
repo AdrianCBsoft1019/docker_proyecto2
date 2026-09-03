4cddd8727e50ed1c1f247cc62e716c393b880b24
Ubuntu <ubuntu@ip-172-31-20-240.us-east-2.compute.internal>
2026-09-03 05:47:01 +0000

    fix: corregir puerto Flask de 5050 a 5000

--- simple_app.py
+++ simple_app.py
@@ -89,5 +89,5 @@ def version():
 
 if __name__ == '__main__':
     debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
-    app.run(host="0.0.0.0", port=5050, debug=debug_mode) # nosec B104
+    app.run(host="0.0.0.0", port=5000, debug=debug_mode) # nosec B104
 
